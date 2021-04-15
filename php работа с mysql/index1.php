309524123453abe6df507e927f85ad785a853768
danil fateev <rfney234@gmail.com>
2021-04-15 15:14:48 +0600

    initial commit

---
+++ php работа с mysql/index1.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<?php
+$link = mysqli_connect("localhost","root","","test");
+if(!$link)
+	{	die('Все плохо');}
+?>.' ?' .
---
+++ php работа с mysql/reg.php
@@ -0,0 +1,10 @@
+<?php 
+	require_once 'inde1.php';
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	mysql_query($link,"insert into users values ");
+
+
+
+
+ ?>
